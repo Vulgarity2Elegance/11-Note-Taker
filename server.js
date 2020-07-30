05833849e7951932334d6c3fd601bb535b7e4ade
Youzhi Hu <jyzhu0101@gmail.com>
2020-07-30 11:39:22 +1000

    server.js points to two routes: htmlRoutes and apiRoutes.

---
+++ server.js
@@ -0,0 +1,19 @@
+// DEPENDENCIES
+const express = require("express");
+
+// EXPRESS CONFIGURATION
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+require("./routes/htmlNotes")(app);
+require("./routes/apiNotes")(app);
+
+// LISTENER
+app.listen(PORT, () => {
+    console.log("App listening on PORT: " + PORT);
+});
