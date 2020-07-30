fb21646bc271e0736cc7833a7389dedd8b293d53
Youzhi Hu <jyzhu0101@gmail.com>
2020-07-30 14:09:42 +1000

    Serving static files in Express.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require("express");
+const path = require("path");
 
 // EXPRESS CONFIGURATION
 const app = express();
@@ -8,10 +9,11 @@ const PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname + "/public")));
 
 // ROUTER
 require("./routes/htmlNotes")(app);
-require("./routes/apiNotes")(app);
+// require("./routes/apiNotes")(app);
 
 // LISTENER
 app.listen(PORT, () => {
