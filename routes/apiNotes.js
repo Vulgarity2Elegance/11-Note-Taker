d31341e4d7781ceb0b586bdd59c5575746f351cc
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-01 17:06:07 +1000

    Fixed the id property in POST and DELETE api routes.

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -6,14 +6,18 @@ module.exports = (app) => {
 
     // POST
     app.post("/api/notes", (req, res) => {
-        dbJSON.push(req.body); // save new note to request body
-        res.json(true); // return the new note to the client
+        const lastId = dbJSON.length // if (dbJson.length === true)
+            ? Math.max(...dbJSON.map((note) => note.id)) // lastId = the maximum one from the note array based on ids
+            : 0; // or the lastId = 0
+        const id = lastId + 1; // id cannot be null
+        dbJSON.push({ id, ...req.body }); // push id and the rest req.body objects to dbJson
+        res.json(dbJSON.slice(-1)); // return the selected element as a new array object ==> new note to the client
     });
 
     // DELETE
     app.delete("/api/notes/:id", (req, res) => {
-        const id = req.params.id; // give each note a unique `id` when it's saved
-        dbJSON.splice(id, 1); // use splice() to remove elements without leaving "holes" in the array
+        let note = dbJSON.find(({ id }) => id === JSON.parse(req.params.id)); // read all notes from the `db.json` file
+        dbJSON.splice(dbJSON.indexOf(note), 1); // remove the note with the given `id` property
         res.json(dbJSON); // rewrite the notes to the `db.json` file
     });
 };
