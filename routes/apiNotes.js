cdc5fc952eb9ae36f63b77a1095cbe57a85830c5
Youzhi Hu <jyzhu0101@gmail.com>
2020-08-01 14:14:32 +1000

    POST and DELETE API routes have been created and running on the server.

--- routes/apiNotes.js
+++ routes/apiNotes.js
@@ -1,5 +1,19 @@
 const dbJSON = require("../db/db.json");
 
 module.exports = (app) => {
+    // GET
     app.get("/api/notes", (req, res) => res.json(dbJSON));
+
+    // POST
+    app.post("/api/notes", (req, res) => {
+        dbJSON.push(req.body); // save new note to request body
+        res.json(true); // return the new note to the client
+    });
+
+    // DELETE
+    app.delete("/api/notes/:id", (req, res) => {
+        const id = req.params.id; // give each note a unique `id` when it's saved
+        dbJSON.splice(id, 1); // use splice() to remove elements without leaving "holes" in the array
+        res.json(dbJSON); // rewrite the notes to the `db.json` file
+    });
 };
